ad3dd1dbcaf316ad04144d48f4575bedfa511f90
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-12 13:19:19 +0200

    parse command

--- main.py
+++ main.py
@@ -5,3 +5,30 @@ import pyttsx3
 import webbrowser
 import wikipedia
 import wolframalpha
+
+# Speech engine initialisation
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)  # 0 ====> male, 1 ====> female
+
+activationWord = 'computer'  # Single word, activation!
+
+
+def parseCommand():
+    listener = sr.Recognizer()
+    print('Listening for a command')
+
+    with sr.Microphone() as source:
+        listener.pause_threshold = 2
+        input_speech = listener.listen(source)
+
+    try:
+        print('Recognizing speech...')
+        query = listener.recognize_google(input_speech, language='en_gb')
+        print(f'The input speech was: {query}')
+    except Exception as exception:
+        print('I did not quite catch that')
+
+        print(exception)
+        return 'None'
+    return query
